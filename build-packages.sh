55af4d992d4d44c1da2fba23715636bd4a02f9d1
goaaats <goatsdev@protonmail.com>
2022-04-16 23:47:24 +0200

    Change package names, publish

--- build-packages.sh
+++ build-packages.sh
@@ -3,6 +3,7 @@ dotnet restore src/Veldrid.sln
 
 export UseStableVersions=true
 
+dotnet pack -c Release src/NativeLibraryLoader/NativeLibraryLoader.csproj
 dotnet pack -c Release src/Veldrid.OpenGLBindings/Veldrid.OpenGLBindings.csproj
 dotnet pack -c Release src/Veldrid.MetalBindings/Veldrid.MetalBindings.csproj
 dotnet pack -c Release src/Veldrid/Veldrid.csproj
